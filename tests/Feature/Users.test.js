cf94d13ab3b92c3dc39264748782fde9f17c3a6a
Huanderson Machado <huandersonmachado@gmail.com>
2021-12-16 18:36:53 -0300

    chore: add faker js

--- tests/Feature/Users.test.js
+++ tests/Feature/Users.test.js
@@ -1,24 +1,29 @@
 const supertest = require('supertest');
+const faker = require('faker');
 const server = require('../../src/server');
 const { User } = require('../../src/app/models');
 
+const generateFakeUser = () => ({
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+});
+
 describe('Test Users Module', () => {
   it('should register new user', async () => {
+    const userFake = generateFakeUser();
+
     const response = await supertest(server)
       .post('/users/register')
       .set('Accept', 'application/json')
       .set('Content-Type', 'application/json')
-      .send({
-        name: 'Huanderson',
-        email: 'huandersonmachado@gmail.com',
-        password: '123456',
-      });
+      .send(userFake);
 
     const user = await User.findOne({
-      where: { email: 'huandersonmachado@gmail.com' },
+      where: { email: userFake.email },
     });
 
-    expect(user.dataValues.name).toBe('Huanderson');
+    expect(user.dataValues.name).toBe(userFake.name);
     expect(response.status).toBe(201);
     expect(response.body).toHaveProperty('message');
     expect(response.body).toHaveProperty('user');
