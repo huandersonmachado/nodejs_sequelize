f182742d3e23849e827c48fd148245c934ee04e3
Huanderson Machado <huandersonmachado@gmail.com>
2021-12-19 15:58:29 -0300

    chore: sites migration

---
+++ src/database/migrations/20211219184441-create-sites.js
@@ -0,0 +1,40 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) =>
+    queryInterface.createTable('sites', {
+      id: {
+        type: Sequelize.DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+      },
+      site_name: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+      link_url: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.DataTypes.UUID,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      created_at: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false,
+      },
+    }),
+
+  down: async (queryInterface) => {
+    queryInterface.removeIndex('sites', 'user_id');
+    queryInterface.dropTable('sites', 'user_id');
+  },
+};
